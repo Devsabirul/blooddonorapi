c2059d2f0d10013ea6ddbb93c0234bbf06a16eef
Shishir Islam shanto <shishir8089@gmail.com>
2022-11-05 14:02:10 +0600

    add is_available a

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'email', 'password',
-                  'is_admin', 'is_donor', 'date_joined', 'last_login')
+                  'is_admin', 'is_donor', 'date_joined', 'last_login', 'is_available')
