d089ad06272e50e6940bd6e2365911ac4bbd1d9c
Shishir Islam shanto <shishir8089@gmail.com>
2022-11-08 01:29:54 +0600

    add favicon.ico

--- complateapi/urls.py
+++ complateapi/urls.py
@@ -3,11 +3,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('api', include('accounts.urls'))
+    path('/', include('core.urls')),
+    path('api', include('accounts.urls')),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('images/favicon.ico')))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
