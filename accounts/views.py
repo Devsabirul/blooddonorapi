6aed7c5124f2f3633f0e3d44a27fdf95b6876e7e
Shishir Islam shanto <shishir8089@gmail.com>
2022-11-08 19:59:42 +0600

    erors add

--- accounts/views.py
+++ accounts/views.py
@@ -54,7 +54,7 @@ class UserRegistrationView(APIView):
         if serializers.is_valid():
             serializers.save()
             return Response({'status': 'success', 'msg': 'Account created successfully'}, status=status.HTTP_201_CREATED)
-        return Response({'errors': 'something wrong'}, status=status.HTTP_409_CONFLICT)
+        return Response({'status': 'failed', 'errors': serializers.errors}, status=status.HTTP_409_CONFLICT)
 
 
 class UserLoginApiView(APIView):
