d00e0245a552b3d4a1decce2c7857ffdd5fc00b5
Shishir Islam shanto <shishir8089@gmail.com>
2022-11-08 20:52:27 +0600

    change more

--- accounts/manager.py
+++ accounts/manager.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.hashers import make_password
-
+'''
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -19,3 +19,25 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_admin', True)
         return self.create_user(email, password, **extra_fields)
+
+'''
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, is_admin, is_donor, is_available, password=None, password2=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(email=self.normalize_email(email), is_admin=is_admin,
+                          is_donor=is_donor, is_available=is_available, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, password2=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_admin', True)
+        return self.create_user(email, password, password2, **extra_fields)
